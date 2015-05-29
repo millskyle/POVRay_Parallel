e3d29b8b2d9bec926d771ebc2f8b50c704ab474b
Kyle Mills <kyle.mills@uoit.net>
2015-05-29 08:12:19 -0400

    added sample cluster skeleton

---
+++ render_template/script_skeletons/sample_cluster.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#PBS -l walltime=08:00:00
+#PBS -l pmem=2000mb
+#PBS -r n
+#PBS -l procs=8
+
+#PBS -N "!!!COUNT!!!_!!!INPUTFILE!!!_render"
+
+cd $PBS_O_WORKDIR
+
+echo -n "START " > datefile
+date >> datefile
+
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/boost/lib:/global/software/gcc-rh6/boost-1.54.0-gcc44/lib
+
+!!!POVRAY_COMMAND!!!  
+
+
+date >> datefile
+
+
+
+
+
+
+
