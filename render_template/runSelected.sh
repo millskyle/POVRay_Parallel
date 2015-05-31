6e0fb196ce4f155f1af523341d636fa518cd52d4
Kyle Mills <kyle.mills@uoit.net>
2015-05-31 10:56:15 -0400

    runSelected.sh now sources config

--- render_template/runSelected.sh
+++ render_template/runSelected.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+source config.sh
+
+
 if [[ -a found.list ]] ; then
    for i in  $(cat found.list) ; do
      cd render_scripts/job$i &&
-     qsub  run_$i.sh   &&
+     $script_run_command  run_$i.sh   &&
      cd ../..
    done
 fi
